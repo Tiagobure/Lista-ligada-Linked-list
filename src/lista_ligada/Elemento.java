98d83ae9bb0ada4b35e3cb8a78283d62b57ee564
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-01 21:54:25 -0300

    First commit linked list

---
+++ src/lista_ligada/Elemento.java
@@ -0,0 +1,22 @@
+package lista_ligada;
+
+public class Elemento {
+
+	// elemento is node--
+	private String valor;
+	private Elemento proximo;
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public Elemento getProximo() {
+		return proximo;
+	}
+	public void setProximo(Elemento proximo) {
+		this.proximo = proximo;
+	}
+	
+	
+}
---
+++ src/lista_ligada/ListaLigada.java
@@ -0,0 +1,44 @@
+package lista_ligada;
+
+public class ListaLigada {
+
+	private Elemento first;
+	private Elemento last;
+	private int size;
+
+	public Elemento getFirst() {
+		return first;
+	}
+
+	public void setFirst(Elemento first) {
+		this.first = first;
+	}
+
+	public Elemento getLast() {
+		return last;
+	}
+
+	public void setLast(Elemento last) {
+		this.last = last;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	public void toAdd(String newValue) {
+		
+	}
+	public void remove(String newValue) {
+		
+	}
+
+	public Elemento get(int position) {
+		return null;
+	} 
+	
+}
